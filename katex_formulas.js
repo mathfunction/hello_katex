9f1dc2667c3b07c8b0fbf778c5f51607dc72c0a8
tommy_cheng <tommy_cheng@bb.com>
2019-09-16 13:49:30 +0800

    0916 fix Gauss

--- katex_formulas.js
+++ katex_formulas.js
@@ -45,7 +45,7 @@ function change_dynamic(){
 	}else if(change_dynamic.state %4 == 2){
 		katex.render("\\approx \\displaystyle{\\underbrace{ \\frac{1}{n} \\sum_{i = 1}^{n}{f(U_i)} \\quad \\text{where } U_i \\overset{i.i.d}{\\sim }\\text{uniform}(0,1) }_{\\text{Monte Carlo Simulation}} }",document.getElementById("dynamic_formula"));
 	}else{ //change_dynamic.state %4 == 0
-		katex.render("\\approx \\displaystyle{\\underbrace{\\frac{1}{2}\\sum_{i=1}^{n}\\omega_{i} f\\bigg(\\frac{x_i+1}{2} \\bigg) \\quad \\text{where }  \\omega_{i} = \\frac{2}{(1-x_i^2) P'_{n}(x_i)} \\text{ and find optimal abscissa } x_i \\text{ s.t } P_n(x_{i})= 0  }_{\\text{Gauss–Legendre Quadrature with } x_i \\in [-1,1] \\text{ and } P_n := \\text{Legendre Polynomial of Degree } n } }",document.getElementById("dynamic_formula"));
+		katex.render("\\approx \\displaystyle{\\underbrace{\\frac{1}{2}\\sum_{i=1}^{n}\\omega_{i} f\\bigg(\\frac{x_i+1}{2} \\bigg) \\quad \\text{where }  \\omega_{i} = \\frac{2}{(1-x_i^2) {[P'_{n}(x_i)]}^2} \\text{ and find optimal abscissa } x_i \\text{ s.t } P_n(x_{i})= 0  }_{\\text{Gauss–Legendre Quadrature with } x_i \\in [-1,1] \\text{ and } P_n := \\text{Legendre Polynomial of Degree } n } }",document.getElementById("dynamic_formula"));
 	}//end_else
 	change_dynamic.state+=1
 }
