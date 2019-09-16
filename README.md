8f0f04748884cba55c5cac044c73cb4b1db62b21
tommy_cheng <tommy_cheng@bb.com>
2019-09-16 13:32:33 +0800

    0916afternoon

---
+++ README.md
@@ -0,0 +1,53 @@
+# Javascript + Katex = latex on browser 範例 !!
+
+### Katex 使用方法概述:
+
+1. 把整個 katex 資料夾複製到你要的目錄， [YourPath] = 資料夾絕對路徑    
+
+2. 在 html-head 裡引入兩個 js 檔案( katex.min.js , auto-render.min.js )，一個 css 檔案 ( katex.min.css ) ，此為本地端 = 離線還可用方法，其它引入 katex 方法可看官網 !!
+
+   ```html
+   <head>
+   	<script src="[YourPath]/katex/katex.min.js"></script>
+   	<script src="[YourPath]/katex/contrib/auto-render.min.js"> </script>
+   	<link type="text/css" rel="stylesheet" href="[YourPath]/katex/katex.min.css"></script>
+   </head>
+   ```
+
+
+3. 在 html-body想顯示的公式的地方創建 \<span\> 標籤，並自行給它 id 
+
+   ```html
+   <body>
+       <span id="my_first_formula"> </span>
+       
+   </body>
+   ```
+
+4. 在 javascript 程式碼 區塊，使用 katex.render 函式，即可顯示數學式 !!
+
+   ```javascript
+   katex.render("Your Latex Code",document.getElementById("my_first_formula"))
+   
+   // document.getElementById 為常見 javascript 語法，可自行 Google !!
+   // "Your Latex Code" 是你要寫的 latex 字串 (不需要包$,$$) 
+   // 需要跳脫字元，所以 latex某些語法如: 非單斜線 \sum 需要雙斜線 \\sum 
+   ```
+
+### 範例碼: 
+
+​	解壓縮後，用瀏覽器打開 hello_katex.html 會出現效果 !! 
+
+​	範例實作可參考 hello_katex.html , katex_formulas.js ，可有按鈕互動顯示功能 !!
+
+### 與 MathJax 比較:  
+
+1.  Katex 顯示效能比較快(感覺順暢) !!
+2.  Katex 顯示樣式比較好看 !!
+3.  MathJax 支援 latex 語法集比較多!!
+
+## 相關連結: 
+
+Katex 官網: https://katex.org/
+
+MathJax 官網: <https://www.mathjax.org/>
---
+++ katex_formulas.js
@@ -0,0 +1,51 @@
+/*====================================================================
+	這是 katex 範例 :
+
+		使用 katex_render_single 更改動態的物件
+		使用 katex_render_all 更改靜態的物件
+
+
+======================================================================*/
+
+function katex_render_single(_id,_str){
+	katex.render(_str,document.getElementById(_id));
+}
+
+
+function katex_render_all(_dict){
+	for(var _id in _dict){
+		// value is latex-string
+		katex.render(_dict[_id], document.getElementById(_id));
+	}//endfor
+}//_katex_rander
+//==========================================================================
+
+
+// ===== define dictionary ============  // 
+
+katex_render_all(
+	{	
+		"constant_formula":"\\underbrace{\\displaystyle{\\int^{1}_{0}f(x)dx}}_{\\text{Definite Integral of } f}  ",
+		"dynamic_formula":" = ??"
+	}
+);
+
+
+function change_dynamic(){
+	// 初始化按鈕的狀態 (宣告 state variable)
+	if(typeof change_dynamic.state == 'undefined' ) {
+        change_dynamic.state = 0;
+    }//endif
+	
+	// 利用同餘運算決定按鈕後的情況
+	if(change_dynamic.state %4 == 0){
+		katex.render("= \\displaystyle{\\underbrace{\\lim_{n \\rightarrow \\infty } \\frac{1}{n} \\cdot \\bigg[\\sum_{k=1}^{n} f\\left(\\frac{k}{n}\\right)  \\bigg] }_{\\text{Take Limit of Series (Right-Riemann-Sum)}}  }",document.getElementById("dynamic_formula"));
+	}else if(change_dynamic.state %4 == 1){
+		katex.render("= \\displaystyle{\\underbrace{F(1)-F(0) \\quad \\text{where } \\text{ closed-form } F \\text{ with } F' = f }_{\\text{2nd Fundamental Thm. of Calculus}} }",document.getElementById("dynamic_formula"));
+	}else if(change_dynamic.state %4 == 2){
+		katex.render("\\approx \\displaystyle{\\underbrace{ \\frac{1}{n} \\sum_{i = 1}^{n}{f(U_i)} \\quad \\text{where } U_i \\overset{i.i.d}{\\sim }\\text{uniform}(0,1) }_{\\text{Monte Carlo Simulation}} }",document.getElementById("dynamic_formula"));
+	}else{ //change_dynamic.state %4 == 0
+		katex.render("\\approx \\displaystyle{\\underbrace{\\frac{1}{2}\\sum_{i=1}^{n}\\omega_{i} f\\bigg(\\frac{x_i+1}{2} \\bigg) \\quad \\text{where }  \\omega_{i} = \\frac{2}{(1-x_i^2) P'_{n}(x_i)} \\text{ and find optimal abscissa } x_i \\text{ s.t } P_n(x_{i})= 0  }_{\\text{Gauss–Legendre Quadrature with } x_i \\in [-1,1] \\text{ and } P_n := \\text{Legendre Polynomial of Degree } n } }",document.getElementById("dynamic_formula"));
+	}//end_else
+	change_dynamic.state+=1
+}
